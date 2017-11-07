67299ff03b1249dd811c364d8b2db3490038e48b
vijaysraj <vijaysraj006@gmail.com>
2017-11-07 10:50:35 +0530

    fixes

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,8 +1,8 @@
-require 'pusher'
+# require 'pusher'
 
-Pusher.app_id = '426731'
-Pusher.key = 'f7fc554df3fe445f182f'
-Pusher.secret = '24018ad0756a71030ad7'
-Pusher.cluster = 'ap2'
-Pusher.logger = Rails.logger
-Pusher.encrypted = true
+# Pusher.app_id = '426731'
+# Pusher.key = 'f7fc554df3fe445f182f'
+# Pusher.secret = '24018ad0756a71030ad7'
+# Pusher.cluster = 'ap2'
+# Pusher.logger = Rails.logger
+# Pusher.encrypted = true
